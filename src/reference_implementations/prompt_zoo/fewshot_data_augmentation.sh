3b121bbbb403be90d0e24a7590e49f792ccbaea5
snajafi <snajafi@v2.cluster.local>
2023-06-14 17:27:47 -0400

    clean up train scripts

---
+++ src/reference_implementations/prompt_zoo/fewshot_data_augmentation.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# For reading key=value arguments
+for ARGUMENT in "$@"
+do
+	KEY=$(echo $ARGUMENT | cut -f1 -d=)
+	KEY_LENGTH=${#KEY}
+	VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+	export "$KEY"="$VALUE"
+done
+
+PROJECT_DIR=$( dirname -- "$0"; )
+
+# We source to keep the internal env variables defined.
+source ${PROJECT_DIR}/../setup_gpu_worker.sh
+
+LEARN_RATE=${LR}
+EXPERIMENT_TYPE=${EXP_TYPE}
+RANDOM_SEED=${SEED}
+TASK_NAME=${TASK}
+NUM_CLASSES=${NUM_CLASSES}
+FEWSHOT_SIZE=${FEWSHOT_SIZE}
+DATA_AUG=${AUG}
+TRAIN_PARAPHRASER=${TRAIN_PARA}
+LOAD_PARAPHRASER=${LOAD_PARA}
+SLURM_JOB_ID=${SLURM_JOB_ID}
+METRIC_TO_SAVE=${METRIC_TO_SAVE}
+PROMPT_LEN=${LEN}
+
+checkpoint_path=/checkpoint/$USER/${SLURM_JOB_ID}
+
+experiment_name=${TASK_NAME}_${NUM_CLASSES}_${FEWSHOT_SIZE}_${EXPERIMENT_TYPE}_${RANDOM_SEED}
+experiment_name=${experiment_name}_${LEARN_RATE}_${PROMPT_LEN}_${DATA_AUG}_${TRAIN_PARAPHRASER}_${LOAD_PARAPHRASER}_${METRIC_TO_SAVE}
+
+model_path=${checkpoint_path}/${experiment_name}
+
+mkdir -p ${model_path}
+
+# delay purge in the checkpoint and job_id
+touch ${checkpoint_path}/DELAYPURGE
+
+if [ "${TASK_NAME}" = "sst2" ]; then
+
+    instruction_type="manual_template_research_sst2_with_instruction"
+    train_batch_size=8
+    if [ "${EXPERIMENT_TYPE}" = "gradient_search" ]; then
+        instruction_type="manual_template_research_sst2_no_instruction"
+        train_batch_size=2
+    fi
+
+elif [ "${TASK_NAME}" = "SetFit/sst5" ]; then
+    instruction_type="manual_template_research_sst5_with_instruction"
+    train_batch_size=8
+    if [ "${EXPERIMENT_TYPE}" = "gradient_search" ]; then
+        instruction_type="manual_template_research_sst5_no_instruction"
+        train_batch_size=2
+    fi
+fi
+
+# train phase
+python -m src.reference_implementations.prompt_zoo.trainer \
+    --train_batch_size ${train_batch_size} \
+    --eval_batch_size 8 \
+    --mode train \
+    --seed ${RANDOM_SEED} \
+    --task_name ${TASK_NAME} \
+    --train_file train \
+    --dev_file validation \
+    --classification_type fewshot \
+    --num_classes ${NUM_CLASSES} \
+    --fewshot_sample_size ${FEWSHOT_SIZE} \
+    --exp_type ${EXPERIMENT_TYPE} \
+    --model_path ${model_path} \
+    --checkpoint best_step \
+    --max_epochs 50 \
+    --learning_rate ${LEARN_RATE} \
+    --training_steps 1000000 \
+    --steps_per_checkpoint 8 \
+    --source_max_length 128 \
+    --decoder_max_length 128 \
+    --weight_decay_rate 0.01 \
+    --instruction_type ${instruction_type} \
+    --pretrained_model roberta-large \
+    --enable_data_augmentation ${DATA_AUG} \
+    --enable_paraphrase_training ${TRAIN_PARAPHRASER} \
+    --load_paraphraser ${LOAD_PARAPHRASER} \
+    --ensemble_type paraphrase_predict \
+    --test_temperature 1.0 \
+    --test_sample_size 8 \
+    --metric_to_save ${METRIC_TO_SAVE} \
+    --g_beam_size 1 \
+    --top_k 20
+
+# test phase
+python -m src.reference_implementations.prompt_zoo.trainer \
+    --eval_batch_size 8 \
+    --mode test \
+    --seed ${RANDOM_SEED} \
+    --task_name ${TASK_NAME} \
+    --test_file validation \
+    --num_classes ${NUM_CLASSES} \
+    --fewshot_sample_size ${FEWSHOT_SIZE} \
+    --exp_type ${EXPERIMENT_TYPE} \
+    --model_path ${model_path} \
+    --para_model_path ${model_path} \
+    --source_max_length 128 \
+    --decoder_max_length 128 \
+    --checkpoint best_step \
+    --prediction_file ${model_path}/${TASK_NAME}.validation.with_instruction.${EXPERIMENT_TYPE}.all_predictions.predictions.csv \
+    --instruction_type ${instruction_type} \
+    --pretrained_model roberta-large \
+    --enable_data_augmentation ${DATA_AUG} \
+    --enable_paraphrase_training ${TRAIN_PARAPHRASER} \
+    --load_paraphraser ${LOAD_PARAPHRASER} \
+    --ensemble_type paraphrase_predict \
+    --test_temperature 1.0 \
+    --test_sample_size 8 \
+    --g_beam_size 1 \
+    --top_k 20
--- src/reference_implementations/prompt_zoo/metrics.py
+++ src/reference_implementations/prompt_zoo/metrics.py
@@ -5,6 +5,8 @@ from typing import Dict
 import numpy as np
 import pandas as pd
 from absl import flags
+from scipy.stats import entropy
+from sklearn.metrics import balanced_accuracy_score
 
 FLAGS = flags.FLAGS
 
@@ -93,3 +95,36 @@ def sentiment_metric(prediction_file: str) -> Dict[str, float]:
         return_metrics["all_accuracy"] = all_accuracy
 
     return return_metrics
+
+
+def grips_sentiment_metric(prediction_file: str) -> float:
+    """Compute the balanced accuracy + entropy for sentiment classification
+    used in grips training."""
+    # pick the class with the highest score among the possible class labels!
+    df = pd.read_csv(prediction_file, delimiter=",")
+    gold_labels = [label.strip() for label in df["gold_class"].tolist()]
+    num_labels = len(set(gold_labels))
+    golds = np.array(gold_labels).reshape((len(gold_labels) // num_labels, num_labels))[:, 0]
+
+    # This relies on the assumption that there is a prediction score for every label. (i.e. n label scores per input)
+    predictions = [label.strip(" </s>") for label in df["potential_class"].tolist()]
+    scores = df["prediction_score"].tolist()
+
+    assert len(predictions) % num_labels == 0
+    prediction_labels = np.array(predictions).reshape((len(predictions) // num_labels, num_labels))
+    prediction_scores = np.array(scores).reshape((len(predictions) // num_labels, num_labels))
+    max_predictions = np.argmax(prediction_scores, axis=1)
+    max_labels = prediction_labels[:, max_predictions][0]
+
+    per_label_correct = {g_label: 0 for g_label in list(set(gold_labels))}
+    total = 0.0
+    for index, gold in enumerate(golds):
+        total += 1.0
+        if gold == max_labels[index]:
+            per_label_correct[gold] += 1
+
+    per_label_frequencies = [count / total for count in per_label_correct.values()]
+    balanced_acc = balanced_accuracy_score(y_true=golds, y_pred=max_labels)
+
+    # 10 is a factor used in the grips implementation.
+    return np.round(100 * balanced_acc, 2) + 10 * entropy(np.array(per_label_frequencies))
