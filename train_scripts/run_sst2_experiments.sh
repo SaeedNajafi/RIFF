0a54dcf8921ed495224bbe919a4681b7973329a8
Saeed Najafi <snajafi@ualberta.ca>
2023-06-20 17:02:29 +0000

    save the sst2 runs

--- train_scripts/run_sst2_experiments.sh
+++ train_scripts/run_sst2_experiments.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+:'
 rates=(0.00001)
 exps=(all_finetune)
 seeds=(11 42 1993 2023 12321)
 tasks=(sst2)
-augs=(0 1)
+augs=(1)
 fewshot_sizes=(32)
 
 for i in ${!rates[@]};
 do
-	rate=${rates[$i]}
+    rate=${rates[$i]}
     for j in ${!exps[@]};
     do
         exp=${exps[$j]}
@@ -25,24 +26,22 @@ do
                     for f in ${!fewshot_sizes[@]};
                     do
                         fewshot_size=${fewshot_sizes[$f]}
-                        sbatch  src/reference_implementations/run_singlenode_prompt.slrm \
-                            src/reference_implementations/prompt_zoo/fewshot_data_augmentation.sh \
-                            ./roberta-exps-logs \
-                            ${exp} \
-                            ${task} \
-                            ${seed} \
-                            2 \
-                            ${fewshot_size} \
-                            ${rate} \
-                            ${aug} \
-                            0 \
-                            0 \
-                            25 \
-                            mml \
-                            off_policy \
-                            beam_search \
-                            original_accuracy \
-                            0.6
+                        CUDA_VISIBLE_DEVICES=1 bash src/reference_implementations/prompt_zoo/fewshot_lmfps.sh \
+                                EXP_TYPE=${exp} \
+                                TASK=${task} \
+                                SEED=${seed} \
+                                NUM_CLASSES=2 \
+                                FEWSHOT_SIZE=${fewshot_size} \
+                                LR=${rate} \
+                                AUG=${aug} \
+                                TRAIN_PARA=0 \
+                                LOAD_PARA=0 \
+                                LEN=25 \
+                                PARA_LOSS="dummy" \
+                                SAMPLING_METHOD="dummy" \
+                                SAMPLING_ALG="dummy" \
+                                METRIC_TO_SAVE=original_accuracy \
+                                KL_COEFFICIENT=0.0
                     done
                 done
             done
@@ -75,30 +74,29 @@ do
                     for f in ${!fewshot_sizes[@]};
                     do
                         fewshot_size=${fewshot_sizes[$f]}
-                        sbatch  src/reference_implementations/run_singlenode_prompt.slrm \
-                            src/reference_implementations/prompt_zoo/fewshot_data_augmentation.sh \
-                            ./roberta-exps-logs \
-                            ${exp} \
-                            ${task} \
-                            ${seed} \
-                            2 \
-                            ${fewshot_size} \
-                            ${rate} \
-                            ${aug} \
-                            0 \
-                            0 \
-                            25 \
-                            mml \
-                            off_policy \
-                            beam_search \
-                            original_accuracy \
-                            0.6
+                        CUDA_VISIBLE_DEVICES=2 bash src/reference_implementations/prompt_zoo/fewshot_lmfps.sh \
+                                EXP_TYPE=${exp} \
+                                TASK=${task} \
+                                SEED=${seed} \
+                                NUM_CLASSES=2 \
+                                FEWSHOT_SIZE=${fewshot_size} \
+                                LR=${rate} \
+                                AUG=${aug} \
+                                TRAIN_PARA=0 \
+                                LOAD_PARA=0 \
+                                LEN=25 \
+                                PARA_LOSS="dummy" \
+                                SAMPLING_METHOD="dummy" \
+                                SAMPLING_ALG="dummy" \
+                                METRIC_TO_SAVE=original_accuracy \
+                                KL_COEFFICIENT=0.0
                     done
                 done
             done
         done
     done
 done
+'
 
 rates=(0.001)
 exps=(soft_prompt_finetune)
@@ -109,7 +107,7 @@ fewshot_sizes=(32)
 
 for i in ${!rates[@]};
 do
-	rate=${rates[$i]}
+    rate=${rates[$i]}
     for j in ${!exps[@]};
     do
         exp=${exps[$j]}
@@ -125,24 +123,22 @@ do
                     for f in ${!fewshot_sizes[@]};
                     do
                         fewshot_size=${fewshot_sizes[$f]}
-                        sbatch  src/reference_implementations/run_singlenode_prompt.slrm \
-                            src/reference_implementations/prompt_zoo/fewshot_data_augmentation.sh \
-                            ./roberta-exps-logs \
-                            ${exp} \
-                            ${task} \
-                            ${seed} \
-                            2 \
-                            ${fewshot_size} \
-                            ${rate} \
-                            ${aug} \
-                            0 \
-                            0 \
-                            25 \
-                            mml \
-                            off_policy \
-                            beam_search \
-                            original_accuracy \
-                            0.6
+                        CUDA_VISIBLE_DEVICES=3 bash src/reference_implementations/prompt_zoo/fewshot_lmfps.sh \
+                                EXP_TYPE=${exp} \
+                                TASK=${task} \
+                                SEED=${seed} \
+                                NUM_CLASSES=2 \
+                                FEWSHOT_SIZE=${fewshot_size} \
+                                LR=${rate} \
+                                AUG=${aug} \
+                                TRAIN_PARA=0 \
+                                LOAD_PARA=0 \
+                                LEN=25 \
+                                PARA_LOSS="dummy" \
+                                SAMPLING_METHOD="dummy" \
+                                SAMPLING_ALG="dummy" \
+                                METRIC_TO_SAVE=original_accuracy \
+                                KL_COEFFICIENT=0.0
                     done
                 done
             done
@@ -159,7 +155,7 @@ fewshot_sizes=(32)
 
 for i in ${!rates[@]};
 do
-	rate=${rates[$i]}
+    rate=${rates[$i]}
     for j in ${!exps[@]};
     do
         exp=${exps[$j]}
@@ -175,24 +171,22 @@ do
                     for f in ${!fewshot_sizes[@]};
                     do
                         fewshot_size=${fewshot_sizes[$f]}
-                        sbatch  src/reference_implementations/run_singlenode_prompt.slrm \
-                            src/reference_implementations/prompt_zoo/fewshot_data_augmentation.sh \
-                            ./roberta-exps-logs \
-                            ${exp} \
-                            ${task} \
-                            ${seed} \
-                            2 \
-                            ${fewshot_size} \
-                            ${rate} \
-                            ${aug} \
-                            0 \
-                            0 \
-                            25 \
-                            mml \
-                            off_policy \
-                            beam_search \
-                            original_accuracy \
-                            0.6
+                        CUDA_VISIBLE_DEVICES=3 bash src/reference_implementations/prompt_zoo/fewshot_lmfps.sh \
+                                EXP_TYPE=${exp} \
+                                TASK=${task} \
+                                SEED=${seed} \
+                                NUM_CLASSES=2 \
+                                FEWSHOT_SIZE=${fewshot_size} \
+                                LR=${rate} \
+                                AUG=${aug} \
+                                TRAIN_PARA=0 \
+                                LOAD_PARA=0 \
+                                LEN=25 \
+                                PARA_LOSS="dummy" \
+                                SAMPLING_METHOD="dummy" \
+                                SAMPLING_ALG="dummy" \
+                                METRIC_TO_SAVE=original_accuracy \
+                                KL_COEFFICIENT=0.0
                     done
                 done
             done
@@ -200,6 +194,7 @@ do
     done
 done
 
+:'
 rates=(0.001)
 exps=(gradient_search)
 seeds=(11 42 1993 2023 12321)
@@ -249,3 +244,4 @@ do
         done
     done
 done
+'
